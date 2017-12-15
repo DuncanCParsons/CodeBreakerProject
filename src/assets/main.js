0ed057cec35fbebfa7d619b9a6e5bce61824ecb3
Duncan Parsons <duncanparsons@selenity.com>
2017-12-15 17:24:15 +0000

    New methods part 1 and 2

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,44 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    console.log(attempt.value);
+    if (attempt.value === '' && answer.value === '')
+    {
+        setHiddenFields();
+    }
+
+    if (!validateInput(input.value))
+    {
+        return false;
+    }
+
+    attempt.value++;
+    
 }
 
-//implement new functions here
+function setHiddenFields() {
+    var number = Math.floor(Math.random() * 10000).toString();
+    number = '0000' + number;
+    if (number.length > 4)
+    {
+        number = number.substring(number.length - 4);
+    }
+    answer.value = number;
+    attempt.value = 0;
+    console.log(number);
+}
+
+function setMessage (message) {
+    document.getElementById('message').innerHTML = message;
+}
+
+function validateInput (input)
+{
+    if (input === undefined || input === null || input.length !== 4)
+    {
+        setMessage('Guesses must be exactly 4 characters long.')
+        return false;
+    }
+
+    return true;
+}
